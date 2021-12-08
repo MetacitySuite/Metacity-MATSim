ba69b75ec162356b72e9cfb63db962b22642c50e
shanataru <shanataru@gmail.com>
2021-12-08 12:55:38 +0100

    Config

--- matsim/run_matsim.sh
+++ matsim/run_matsim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -Xmx60g -cp matsim-14.0-v1.0.0-SNAPSHOT.jar org.matsim.run.Controler data/matsim-files/config-prague.xml
+java -Xmx10g -cp matsim-14.0-v1.0.0-SNAPSHOT.jar org.matsim.run.Controler data/matsim-files/config-prague-run.xml
 
