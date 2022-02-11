f8d5e971d1d416e617522ebe63ba33914eedd082
AnnaMoudra <anna.moudra@gmail.com>
2022-02-11 12:31:11 +0100

    changes to configs

--- matsim/downsample.sh
+++ matsim/downsample.sh
@@ -3,6 +3,6 @@ if [ -z "$1" ]
 then 
 	echo "Empty downsampling scale factor argument!"
 else
-	java -cp matsim-14.0-v1.0.0-SNAPSHOT.jar org.matsim.project.DownsamplePopulation "$1"
+	java -cp matsim-13.0-v1.0.0-SNAPSHOT.jar org.matsim.project.DownsamplePopulation "$1"
 fi
 
